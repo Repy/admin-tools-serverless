937480814bf256a011702edff5f5adae78e4f02c
repy <gamecube02+git@gmail.com>
2019-10-17 09:21:36 +0900

    このまま表示機能

--- src/main/java/info/repy/tools/controller/db/DbCsvController.java
+++ src/main/java/info/repy/tools/controller/db/DbCsvController.java
@@ -4,6 +4,8 @@ import com.opencsv.CSVWriterBuilder;
 import com.opencsv.ICSVWriter;
 import info.repy.tools.controller.config.DbCsvConfig;
 import info.repy.tools.controller.Config;
+import lombok.AllArgsConstructor;
+import lombok.Data;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
@@ -40,6 +42,33 @@ public class DbCsvController {
         return new ModelAndView("db/csv/id").addObject("sql", sqlOpt.get());
     }
 
+    @Data
+    @AllArgsConstructor
+    public static class DBData {
+        List<String> headers = new ArrayList<>();
+        List<List<String>> rows = new ArrayList<>();
+    }
+
+    private DBData data(DbCsvConfig sql){
+        List<String> headers = new ArrayList<>();
+        List<List<String>> rows = this.jdbc.query(sql.getSql(), (rs, rowNum) -> {
+            if (headers.isEmpty()) {
+                ResultSetMetaData meta = rs.getMetaData();
+                for (int i = 0; i < meta.getColumnCount(); i++) {
+                    String name = meta.getColumnLabel(i + 1);
+                    headers.add(name);
+                }
+            }
+            List<String> row = new ArrayList<>();
+            for (int i = 0; i < headers.size(); i++) {
+                String val = rs.getString(i + 1);
+                row.add(val);
+            }
+            return row;
+        });
+        return new DBData(headers, rows);
+    }
+
     @GetMapping(path = "/db/csv/{id}/download", produces = MediaType.APPLICATION_OCTET_STREAM_VALUE)
     @ResponseBody
     public byte[] download(@PathVariable("id") String id) {
@@ -48,36 +77,36 @@ public class DbCsvController {
             return Objects.equals(data.getId(), id);
         }).findFirst();
         DbCsvConfig sql = sqlOpt.get();
+
+        DBData data = this.data(sql);
+
+        // CSV
         StringWriter sw = new StringWriter();
-
-        List<String> header = new ArrayList<>();
-        List<List<String>> data = this.jdbc.query(sql.getSql(), (rs, rowNum) -> {
-            if (header.isEmpty()) {
-                ResultSetMetaData meta = rs.getMetaData();
-                for (int i = 0; i < meta.getColumnCount(); i++) {
-                    String name = meta.getColumnLabel(i + 1);
-                    header.add(name);
-                }
-            }
-            List<String> row = new ArrayList<>();
-            for (int i = 0; i < header.size(); i++) {
-                String val = rs.getString(i + 1);
-                row.add(val);
-            }
-            return row;
-        });
-
         try (
                 ICSVWriter writer = new CSVWriterBuilder(sw).build();
         ) {
-            writer.writeNext(header.toArray(new String[0]));
-            for (List<String> row : data) {
+            writer.writeNext(data.getHeaders().toArray(new String[0]));
+            for (List<String> row : data.getRows()) {
                 writer.writeNext(row.toArray(new String[0]));
             }
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
         return sw.toString().getBytes(Charset.forName("Windows-31J"));
+    }
+
+    @GetMapping(path = "/db/csv/{id}/view", produces = MediaType.APPLICATION_OCTET_STREAM_VALUE)
+    @ResponseBody
+    public ModelAndView view(@PathVariable("id") String id) {
+        List<DbCsvConfig> conf = config.read().getDbCsv();
+        Optional<DbCsvConfig> sqlOpt = conf.stream().filter((data) -> {
+            return Objects.equals(data.getId(), id);
+        }).findFirst();
+        DbCsvConfig sql = sqlOpt.get();
+
+        DBData data = this.data(sql);
+
+        return new ModelAndView("db/csv/view").addObject("sql", sqlOpt.get()).addObject("data", data);
     }
 
 }
